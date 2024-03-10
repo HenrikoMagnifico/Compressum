14755678f02d2f1384a2aba3b6bb8d1aea52c3ad
Henriko Magnifico <henrikomagnifico@gmail.com>
2024-03-11 00:19:23 +0100

    Initial Commit

---
+++ Compressum/CompressumApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompressumApp.swift
+//  Compressum
+//
+//  Created by Henrik Öberg on 2024-03-11.
+//
+
+import SwiftUI
+
+@main
+struct CompressumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Compressum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Compressum
+//
+//  Created by Henrik Öberg on 2024-03-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
